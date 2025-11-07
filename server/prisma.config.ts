0538979fd00a936c68398c5bb2ef70519cb25b10
Yuz167 <yuhaozhang2002@163.com>
2025-11-07 00:01:19 -0800

    Installed Prisma and migrated schemas. Seeded database with mock data.

--- server/prisma.config.ts
+++ server/prisma.config.ts
@@ -1,4 +1,6 @@
 import { defineConfig, env } from "prisma/config";
+import * as dotenv from "dotenv";
+dotenv.config(); // load .env
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
